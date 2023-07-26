5ede50666508813935b73fba85002a22f0b363d7
fauzandp <fauzan27dp@gmail.com>
2023-07-26 11:47:29 +0700

    Project16_SwiftUI | HotProspects App Introduction Understanding Swift’s Result type

---
+++ HotProspects/Introduction/ResultView.swift
@@ -0,0 +1,43 @@
+//
+//  ResultView.swift
+//  HotProspects
+//
+//  Created by Fauzan Dwi Prasetyo on 26/07/23.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    @State private var output = ""
+    
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings."
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Download error: \(error.localizedDescription)"
+        }
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
